af4b5ea8871e8ad4e413d018567906b3d15bed2f
KingRacing <109063325+KingRacing@users.noreply.github.com>
2022-11-02 11:48:48 +0800

    Update kenkan.py

--- kenkan.py
+++ kenkan.py
@@ -92,7 +92,7 @@ DRINKS = [
            
 @bot.on_message(filters.command("start"))
 async def start_message(client: Client, message: Message):
-    await message.reply(f"Selamat bermain dan selamat ter spill!\n\nJangan lupa subs @{CHANNEL} dan contact @{OWNER} untuk info lainnya.\n\nNote : khusus RL bukan RP")
+    await message.reply(f"Selamat bermain spill Or drink {},silakan tekan perintah di bawah ini untuk info bot.\n\/spill untuk spill dulu\n\drink untuk drink\n\donasi untuk membantu owner bot\n\/request untuk request bikinan mu\n\Note : khusus RL bukan RP")
     
 @bot.on_message(filters.command("help"))
 async def help_message(client: Client, message: Message):
